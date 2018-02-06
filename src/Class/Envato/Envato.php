84c559b863cd53822c3a28421cb108a9a4eac402
Payam Khaninejad <khaninejad@gmail.com>
2018-02-06 16:21:04 +0330

    Disable SSL verify on localhost

--- src/Class/Envato/Envato.php
+++ src/Class/Envato/Envato.php
@@ -2,33 +2,36 @@
 
   namespace Smafe;
 
-  class Envato {
+class Envato
+{
 
     /**
     *** Construct authentication details
     **/
 
-    public function __construct( $data ) {
+    public function __construct($data)
+    {
+        $this->endpoint = 'https://api.envato.com/';
 
-      $this->endpoint = 'https://api.envato.com/';
+        if (isset($data['api_id'])) {
+            $this->api_id = $data['api_id'];
+        }
 
-      if( isset( $data['api_id'] ) )
-        $this->api_id = $data['api_id'];
+        if (isset($data['api_secret'])) {
+            $this->api_secret = $data['api_secret'];
+        }
 
-      if( isset( $data['api_secret'] ) )
-        $this->api_secret = $data['api_secret'];
+        if (isset($data['api_redirect'])) {
+            $this->api_redirect = $data['api_redirect'];
+        }
 
-      if( isset( $data['api_redirect'] ) )
-        $this->api_redirect = $data['api_redirect'];
+        if (isset($data['api_token'])) {
+            $this->api_token = $data['api_token'];
+        }
 
-      if( isset( $data['api_token'] ) )
-        $this->api_token = $data['api_token'];
-
-      if( isset( $data['api_refresh_token'] ) ) {
-        $this->api_refresh_token = $data['api_refresh_token'];
-
-      }
-
+        if (isset($data['api_refresh_token'])) {
+            $this->api_refresh_token = $data['api_refresh_token'];
+        }
     }
 
 
@@ -36,10 +39,9 @@
     *** Generate Auth URL
     **/
 
-    public function getAuthUrl() {
-
-      return $this->endpoint . 'authorization?response_type=code&client_id=' . $this->api_id . '&redirect_uri=' . $this->api_redirect;
-
+    public function getAuthUrl()
+    {
+        return $this->endpoint . 'authorization?response_type=code&client_id=' . $this->api_id . '&redirect_uri=' . $this->api_redirect;
     }
 
 
@@ -47,10 +49,9 @@
     *** Request function
     **/
 
-    public function request( $route, $path = 'GET', $data = false ) {
-
-      return self::curl( $route, $path, $data );
-
+    public function request($route, $path = 'GET', $data = false)
+    {
+        return self::curl($route, $path, $data);
     }
 
 
@@ -58,10 +59,9 @@
     *** Look for valid token (Is user logged in)
     **/
 
-    public function validToken() {
-
-      return self::curl( 'v1/market/private/user/email.json', 'GET' );
-
+    public function validToken()
+    {
+        return self::curl('v1/market/private/user/email.json', 'GET');
     }
 
 
@@ -69,10 +69,9 @@
     *** Set access token
     **/
 
-    public function setAccessToken( $token ) {
-
-      $this->user_token = $token;
-
+    public function setAccessToken($token)
+    {
+        $this->user_token = $token;
     }
 
 
@@ -80,48 +79,51 @@
     *** Code to Token
     **/
 
-    public function getAccessToken( $code, $type = 'AUTH', $exceptions = true ) {
+    public function getAccessToken($code, $type = 'AUTH', $exceptions = true)
+    {
+        $ch = curl_init($this->endpoint . 'token');
 
-      $ch = curl_init( $this->endpoint . 'token' );
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-      curl_setopt( $ch, CURLOPT_POST, true );
-      curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
-
-      if( $type == 'AUTH' )
-        curl_setopt( $ch, CURLOPT_POSTFIELDS, http_build_query( array(
+        if ($type == 'AUTH') {
+            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array(
           'client_id' => $this->api_id,
           'redirect_uri' => $this->api_redirect,
           'client_secret' => $this->api_secret,
           'code' => $code,
           'grant_type' => 'authorization_code'
-        ) ) );
-      elseif( $type == 'REFRESH' )
-        curl_setopt( $ch, CURLOPT_POSTFIELDS, http_build_query( array(
+        )));
+        } elseif ($type == 'REFRESH') {
+            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array(
           'client_id' => $this->api_id,
           'client_secret' => $this->api_secret,
           'refresh_token' => $code,
           'grant_type' => 'refresh_token'
-        ) ) );
+        )));
+        }
 
-      $token = json_decode( curl_exec( $ch ) );
+        $token = json_decode(curl_exec($ch));
 
-      // Throw Exception if error exist
-      if( isset( $token->error ) AND $exceptions )
-        throw new \ErrorException( ( isset( $token->error_description ) ) ? $token->error_description : $token->error );
+        // Throw Exception if error exist
+        if (isset($token->error) and $exceptions) {
+            throw new \ErrorException((isset($token->error_description)) ? $token->error_description : $token->error);
+        }
 
-      // Return empty result if error and $exceptions disabled
-      if( isset( $token->error ) AND !$exceptions )
-        return false;
+        // Return empty result if error and $exceptions disabled
+        if (isset($token->error) and !$exceptions) {
+            return false;
+        }
 
-      // Save token if function exists
-      if( function_exists( 'saveAccessToken' ) )
-        saveAccessToken( $token->access_token );
+        // Save token if function exists
+        if (function_exists('saveAccessToken')) {
+            saveAccessToken($token->access_token);
+        }
 
-      // Set current access token in the library
-      self::setAccessToken( $token->access_token );
+        // Set current access token in the library
+        self::setAccessToken($token->access_token);
 
-      return $token;
-
+        return $token;
     }
 
 
@@ -129,35 +131,31 @@
     *** Request code to token
     **/
 
-    function renewToken() {
+    public function renewToken()
+    {
+        if (is_array($this->api_refresh_token)) {
+            $token = $this->getAccessToken(current($this->api_refresh_token), 'REFRESH', false);
 
-      if( is_array( $this->api_refresh_token ) ) {
+            if (!isset($token->access_token) and next($this->api_refresh_token)) {
+                return self::renewToken();
+            }
 
-        $token = $this->getAccessToken( current( $this->api_refresh_token ), 'REFRESH', false );
+            if (!isset($token->access_token) and !next($this->api_refresh_token)) {
+                return false;
+            }
 
-        if( !isset( $token->access_token ) AND next( $this->api_refresh_token ) )
-          return self::renewToken();
+            return true;
+        } elseif ($this->api_refresh_token) {
+            $token = $this->getAccessToken($this->api_refresh_token, 'REFRESH');
 
-        if( !isset( $token->access_token ) AND !next( $this->api_refresh_token ) )
-          return false;
+            if (!$token->access_token) {
+                return false;
+            }
 
-        return true;
-
-      } elseif( $this->api_refresh_token ) {
-
-        $token = $this->getAccessToken( $this->api_refresh_token, 'REFRESH' );
-
-        if( !$token->access_token )
-          return false;
-
-        return true;
-
-      } else {
-
-        return false;
-
-      }
-
+            return true;
+        } else {
+            return false;
+        }
     }
 
 
@@ -165,16 +163,17 @@
     *** Get current live token
     **/
 
-    private function getToken() {
+    private function getToken()
+    {
+        if (isset($this->user_token)) {
+            return $this->user_token;
+        }
 
-      if( isset( $this->user_token ) )
-        return $this->user_token;
+        if (isset($this->api_token)) {
+            return $this->api_token;
+        }
 
-      if( isset( $this->api_token ) )
-        return $this->api_token;
-
-      return false;
-
+        return false;
     }
 
 
@@ -182,65 +181,65 @@
     *** Submit requests using curl
     **/
 
-    private function curl( $endpoint, $method = 'GET', $data = false, $retry = false ) {
+    private function curl($endpoint, $method = 'GET', $data = false, $retry = false)
+    {
+        if ($method == 'GET' and is_array($data)) {
+            $filter = '?' . http_build_query($data);
+        }
 
-      if( $method == 'GET' AND is_array( $data) )
-        $filter = '?' . http_build_query( $data );
+        $sort = (isset($filter)) ? $filter : '';
 
-      $sort = ( isset( $filter ) ) ? $filter : '';
+        $curl = curl_init($this->endpoint . $endpoint . $sort);
 
-      $curl = curl_init( $this->endpoint . $endpoint . $sort );
+        $data_j = json_encode($data);
 
-      $data_j = json_encode( $data );
+        curl_setopt($curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array( 'Content-Type: application/json' , 'Authorization: Bearer ' . self::getToken() ));
 
-      curl_setopt( $curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4 );
-      curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
-      curl_setopt( $curl, CURLOPT_HTTPHEADER, array( 'Content-Type: application/json' , 'Authorization: Bearer ' . self::getToken() ) );
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
 
-      if( $method == 'POST' ) {
-        curl_setopt( $curl, CURLOPT_POST, true );
-        curl_setopt( $curl, CURLOPT_POSTFIELDS, $data_j );
-      }
+        if ($method == 'POST') {
+            curl_setopt($curl, CURLOPT_POST, true);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $data_j);
+        }
 
-      if( $method == 'PUT' ) {
-        curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
-        curl_setopt( $curl, CURLOPT_CUSTOMREQUEST, 'PUT' );
-        curl_setopt( $curl, CURLOPT_POSTFIELDS, $data_j );
-      }
+        if ($method == 'PUT') {
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'PUT');
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $data_j);
+        }
 
-      $result = json_decode( curl_exec( $curl ) );
+        $result = json_decode(curl_exec($curl));
 
-      if( curl_error( $curl ) )
-        throw new \ErrorException( curl_error( $curl ) );
+        if (curl_error($curl)) {
+            throw new \ErrorException(curl_error($curl));
+        }
 
-      curl_close( $curl );
+        curl_close($curl);
 
-      // Error & Refresh token? Try again
-      if( ( isset( $result->error ) && isset( $this->api_refresh_token ) && !$retry ) OR $result->message === 'Unauthorized' ) {
+        // Error & Refresh token? Try again
+        if ((isset($result->error) && isset($this->api_refresh_token) && !$retry) or $result->message === 'Unauthorized') {
+            $refresh = self::renewToken();
 
-        $refresh = self::renewToken();
+            if ($refresh) {
+                return self::curl($endpoint, $method, $data, true);
+            }
+        }
 
-        if( $refresh )
-          return self::curl( $endpoint, $method, $data, true );
+        // Throw Exception if error exist
+        if (isset($result->error) and isset($result->error_description)) {
+            throw new \ErrorException($result->error_description);
+        } elseif (isset($result->error) and isset($result->description)) {
+            throw new \ErrorException($result->description);
+        } elseif (isset($result->error)) {
+            throw new \ErrorException($result->error);
+        } elseif (isset($result->message) and $result->message === 'Unauthorized') {
+            throw new \ErrorException($result->message);
+        }
 
-      }
-
-      // Throw Exception if error exist
-      if( isset( $result->error ) AND isset( $result->error_description ) )
-        throw new \ErrorException( $result->error_description );
-
-      elseif( isset( $result->error ) AND isset( $result->description ) )
-        throw new \ErrorException( $result->description );
-
-      elseif( isset( $result->error ) )
-        throw new \ErrorException( $result->error );
-
-      elseif( isset( $result->message ) AND $result->message === 'Unauthorized' )
-        throw new \ErrorException( $result->message );
-
-      // Return JSON decoded result
-      return $result;
-
+        // Return JSON decoded result
+        return $result;
     }
-
-  }
+}
